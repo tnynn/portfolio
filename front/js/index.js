3ed7289cf17b0193ff4ac04467ecf29304e8eda7
mnk-gengar <taniyasonnp@gmail.com>
2022-09-23 09:47:44 +0900

    開発環境_js対応＆index作成

---
+++ front/js/index.js
@@ -0,0 +1,16 @@
+// 名前空間
+var pf = pf || {};
+
+pf.constants = {
+	// 活性
+	ACTIVE_CLASS: 'is-active',
+  // 非表示
+	HIDDEN_CLASS: 'is-hidden'
+};
+
+// controller
+
+// スクロール
+$('.js-nav').click(function(){
+	console.log('test');
+});
